9b29e7397b959b38a97334ae1e4e7447fb2e0d51
kjford <Jaleel@@@94>
2022-04-01 12:50:05 -0500

    Create Badge Seeder for db

---
+++ database/seeders/BadgeSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class BadgeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data =   [
+            ['name'=>'Beginner',     'achievement_count' => 0],
+            ['name'=>'Intermediate', 'achievement_count' => 4],
+            ['name'=>'Advanced',     'achievement_count' => 8],
+            ['name'=>'Master',       'achievement_count' => 10],
+        ];
+
+        Badge::query()->truncate();
+        Badge::query()->insert($data);
+    }
+}
