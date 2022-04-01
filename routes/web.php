eaeabce08436cdf9a0c071ef948669377938a845
kjford <Jaleel@@@94>
2022-04-01 12:46:34 -0500

    Added routes for test

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AchievementsController;
 
 Route::get('/users/{user}/achievements', [AchievementsController::class, 'index']);
+
+Route::post('/users/comment/add', [AchievementsController::class, 'commentRequest']);
+
+Route::post('/users/lesson-watched/add', [AchievementsController::class, 'lessonWatchedRequest']);
