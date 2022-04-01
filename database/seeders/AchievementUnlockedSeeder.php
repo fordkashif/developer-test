a6426e137a53599061dbee134e5f37fbf526ae12
kjford <Jaleel@@@94>
2022-04-01 12:57:53 -0500

    Added achievement seeder and migration scripts

---
+++ database/seeders/AchievementUnlockedSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Achievement;
+use Illuminate\Database\Seeder;
+
+class AchievementUnlockedSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data =   [
+
+            ['name'=>'First Lesson Watched', 'count' => 1, 'type'=>'lesson'],
+            ['name'=>'5 Lessons Watched',    'count' => 5, 'type'=>'lesson'],
+            ['name'=>'10 Lesson Watched',    'count' => 10,'type'=>'lesson'],
+            ['name'=>'25 Lesson Watched',    'count' => 25,'type'=>'lesson'],
+            ['name'=>'50 Lesson Watched',    'count' => 50,'type'=>'lesson'],
+
+            ['name'=>'First Comment Written',    'count' => 1, 'type'=>'comment'],
+            ['name'=>'3 Comments    Written',    'count' => 3, 'type'=>'comment'],
+            ['name'=>'5 Comments    Written',    'count' => 5, 'type'=>'comment'],
+            ['name'=>'10 Comments   Written',    'count' => 10,'type'=>'comment'],
+            ['name'=>'20 Comments   Written',    'count' => 20,'type'=>'comment'],
+
+        ];
+
+
+      Achievement::query()->truncate();
+      Achievement::query()->insert($data);
+    }
+}
--- database/seeders/BadgeSeeder.php
+++ database/seeders/BadgeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Badge;
 use Illuminate\Database\Seeder;
 
 class BadgeSeeder extends Seeder
